4a68d07278162b05d88c1506af0b9f4213c5c68e
Ganesh Rajendra Phadtare <69672721+GaneshPhadtare@users.noreply.github.com>
2025-07-26 17:47:44 +0530

    Header and testimonials

---
+++ src/components/testimonials/testimonials.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials',
+  standalone: true,
+  imports: [],
+  templateUrl: './testimonials.component.html',
+  styleUrl: './testimonials.component.scss'
+})
+export class TestimonialsComponent {
+  isPopupOpen = false;
+  selectedImage: string = '';
+
+    openPopup(image: string) {
+      debugger
+    this.selectedImage = image;
+    this.isPopupOpen = true;
+  }
+
+  closePopup() {
+    this.isPopupOpen = false;
+    this.selectedImage = '';
+  }
+}
