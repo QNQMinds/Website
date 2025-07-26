a3975ae8281af2213d9ad78f434e69c0d4c336a6
rizwan7773 <75516649+rizwan7773@users.noreply.github.com>
2025-07-26 20:48:48 +0530

    ui

--- src/components/home-page/home-page.component.ts
+++ src/components/home-page/home-page.component.ts
@@ -2,8 +2,7 @@ import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { AboutComponent } from '../about/about.component';
 import { CourcesPageComponent } from '../cources-page/cources-page.component';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { OwlOptions } from 'ngx-owl-carousel-o';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { HeaderComponent } from '../../layouts/header/header.component';
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
 import { FooterComponent } from '../../layouts/footer/footer.component';
@@ -11,25 +10,47 @@ import { FooterComponent } from '../../layouts/footer/footer.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [AboutComponent,CourcesPageComponent,CarouselModule,HeaderComponent,TestimonialsComponent,FooterComponent],
+  imports: [AboutComponent,
+    CarouselModule,CourcesPageComponent,HeaderComponent,TestimonialsComponent,FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
   currentSection = 'home';
-  images = [
-    'Riz',
-    'https://picsum.photos/id/1011/900/500',
-    'Gotya'
+  
+
+   customOptions: OwlOptions = {
+    loop: true,
+    margin: 15,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: true,
+    nav: false,
+    items: 1,
+    responsive: {
+      0: { items: 1 }
+    }
+  };
+  testimonials = [
+    {
+      image: '../../assets/images/Daily Vlogger (1).png',
+      text: '1 TO 1 Mentorship'
+    },
+    {
+      image: '../../assets/images/Daily Vlogger.png',
+      text: 'Live Market Trading'
+    },
+    {
+      image: '../../assets/images/Daily Vlogger (2).png',
+      text: 'We are Real Traders'
+    },
+    {
+      image: '../../assets/images/Daily Vlogger (3).png',
+      text: 'Offline Batches'
+    }
   ];
 
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    dots: true,
-    nav: true,
-    items: 1,
-  };
+
 courses = [
   { title: 'Beginner Stock Market', description: 'Learn basics, demat, IPOs, and more.' },
   { title: 'Technical Analysis', description: 'Chart reading, indicators, price action.' },
@@ -44,7 +65,7 @@ courses = [
       el.classList.add('visible');
     }
   });
-    const sections = ['home', 'about', 'course'];
+    const sections = ['home', 'about', 'course','testimonial'];
     for (let section of sections) {
       const element = document.getElementById(section);
       if (element) {
